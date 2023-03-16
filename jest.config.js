37fd4c857763ada80b369f887d19957ab7f2d35f
Gagea Eusebiu Andrei <gageaeusebiuandrei@Gageas-MacBook-Pro.local>
2023-03-17 00:17:12 +0200

    feat: integrate mui

--- jest.config.js
+++ jest.config.js
@@ -21,14 +21,14 @@ const customJestConfig = {
     '!**/*.d.ts',
     '!**/node_modules/**',
   ],
-  coverageThreshold: {
-    global: {
-      branches: 1,
-      functions: 1,
-      lines: 1,
-      statements: 1,
-    },
-  },
+  // coverageThreshold: {
+  //   global: {
+  //     branches: 80,
+  //     functions: 80,
+  //     lines: 80,
+  //     statements: 80,
+  //   },
+  // },
   testEnvironment: 'jest-environment-jsdom',
 };
 
